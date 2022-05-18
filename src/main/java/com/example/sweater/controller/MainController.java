bc0216063486e667cef8eff3b2689938094afa92
19906748 <SAKitavtsev@sberbank.ru>
2022-05-18 12:18:10 +0300

    lesson 13.

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +21,8 @@ import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -52,7 +56,8 @@ public class MainController {
                       @Valid Message message,
                       BindingResult bindingResult,
                       Model model,
-                      @RequestParam("file") MultipartFile file) throws IOException {
+                      @RequestParam("file") MultipartFile file
+    ) throws IOException {
         message.setAuthor(user);
 
         if(bindingResult.hasErrors()){
@@ -61,21 +66,63 @@ public class MainController {
             model.addAttribute("message", message);
         }
         else {
-            if (file != null && !file.getOriginalFilename().isEmpty()) {
-                File uploadDir = new File(uploadPath);
-                if (!uploadDir.exists()) {
-                    uploadDir.mkdir();
-                }
-                String uuidFile = UUID.randomUUID().toString() + "." + file.getOriginalFilename();
-                file.transferTo(new File(uploadPath + "/" + uuidFile));
-                message.setFilename(uuidFile);
-            }
+            saveFile(message, file);
             model.addAttribute("message", null);
             messageRepo.save(message);
         }
         Iterable<Message> messages = messageRepo.findAll();
         model.addAttribute("messages", messages);
         return "main";
+    }
+
+    private void saveFile(Message message, MultipartFile file) throws IOException {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString() + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + uuidFile));
+            message.setFilename(uuidFile);
+        }
+    }
+
+    @GetMapping("/user-messages/{user}")
+    public String getUserMessages(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user,
+            Model model,
+            @RequestParam(required = false) Message message
+    ) {
+        Set<Message> messages = user.getMessages();
+        model.addAttribute("message", message);
+        model.addAttribute("messages", messages);
+        model.addAttribute("isCurrentUser", currentUser.equals(user));
+        return "userMessages";
+    }
+
+    @PostMapping("/user-messages/{userId}")
+    public String editUserMessage(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable Long userId,
+            @RequestParam("id") Message message,
+            @RequestParam("text") String text,
+            @RequestParam("tag") String tag,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+        if(message.getAuthor().equals(currentUser)) {
+            if(!ObjectUtils.isEmpty(text)) {
+                message.setText(text);
+            }
+            if(!ObjectUtils.isEmpty(tag)){
+                message.setTag(tag);
+            }
+            saveFile(message, file);
+        }
+
+        messageRepo.save(message);
+
+        return "redirect:/user-messages/" + userId;
     }
 
 
